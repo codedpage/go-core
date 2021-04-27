cfa9beef772e730a93ed98eca34f7276dbcdf597
manoj <manoj.kumar@pcfinancial.in>
2021-04-27 22:47:09 +0530

    closer func

---
+++ 16-first-class/ex5-closer.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//5 - Closures : a anonymous functions which can access the variables defined outside of function
+	a5 := 5
+	func() {
+		fmt.Println("a5 =", a5)
+	}()
+}
